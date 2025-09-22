ec835dd1899bb5adcfb05999f03cddd46e056f4c
Van Quang <quang.tran.van@airstage-vn.asia>
2025-09-22 18:03:39 +0700

    fix frame

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // applies to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN", // or remove this line to allow all
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
