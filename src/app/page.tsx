cbd9fc161b83fe5db3eb7473be40b9a3d5656ec5
Van Quang <quang.tran.van@airstage-vn.asia>
2025-09-22 20:25:25 +0700

    fix sdk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 "use client";
+const appMode = process.env.NEXT_PUBLIC_DEBUG
+const appUrl = process.env.NEXT_PUBLIC_URL
+const appProNum = process.env.NEXT_PUBLIC_CLOUD_PROJECT_NUMBER
 
 export default function Home() {
+  console.log(appMode, appUrl, appProNum)
   return (
     <main className="min-h-screen bg-gray-900 text-white p-8 font-sans flex flex-col gap-12">
       Hello world
